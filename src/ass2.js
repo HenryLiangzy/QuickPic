ba27222b9bd99dfdaadda5b4d1839f6d292bbac7
Henry Leung <henryliangzy@gmail.com>
2021-03-26 13:45:24 +1100

    modify css and js libary file

--- src/ass2.js
+++ src/ass2.js
@@ -1,7 +1,23 @@
-let signup = document.forms.signup;
+const signup_form = document.forms.signup;
 
-signup.addEventListener('submit', (event)=>{
+
+const checkPassword = (password, confirm_password) => {
+    if(password.length != confirm_password.length ){
+        return 1;
+    }
+
+}
+
+
+signup_form.addEventListener('submit', (event)=>{
+    const username = signup_form.username;
+    const password = signup_form.password;
+    const con_password = signup_form.con_password;
+
     event.preventDefault();
     console.log("Submit function detect!");
+    if (checkPassword(password, con_password) === 1) {
+        alert("Confirm password not match input password!");
+    }
 })
 
