a8b82f71dbfe61290928224eb041382e84aa708d
Henry Leung <henryliangzy@gmail.com>
2021-03-26 18:54:57 +1100

    modal first attempt

--- src/ass2.js
+++ src/ass2.js
@@ -1,23 +1,32 @@
-const signup_form = document.forms.signup;
+let signup_form = document.forms.signup;
+let myModal = new bootstrap.Modal(document.getElementById('myModal'), {backdrop: true});
 
+const showAlert = (alertTitle, alertText) => {
+    myModal.modalTitle = alertTitle;
+    myModal.modalText = alertText;
 
-const checkPassword = (password, confirm_password) => {
-    if(password.length != confirm_password.length ){
-        return 1;
-    }
-
+    myModal.show();
 }
 
+const closeAlert = () => {
+    myModal.hide();
+}
 
 signup_form.addEventListener('submit', (event)=>{
-    const username = signup_form.username;
-    const password = signup_form.password;
-    const con_password = signup_form.con_password;
-
+    
     event.preventDefault();
+    
+    const username = signup_form.elements.username.value;
+    const password = signup_form.elements.password.value;
+    const con_password = signup_form.elements.confirmPassword.value;
+
     console.log("Submit function detect!");
-    if (checkPassword(password, con_password) === 1) {
-        alert("Confirm password not match input password!");
+    console.log(`password length ${password.length}`);
+    console.log(`confirm password length ${con_password.length}`);
+    
+    if (password != con_password) {
+        showAlert("Error", "Confirm password incorrect!");
+        //alert("Confirm password incorrect!");
     }
 })
 
