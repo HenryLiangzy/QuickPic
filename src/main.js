260f107f5066ee78ad19b8694ae431511e2c28ea
Henry Leung <henryliangzy@gmail.com>
2021-03-27 17:26:27 +1100

    resture file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import API from './api.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
+
+let login_form = document.forms.login;
+let myModal = new bootstrap.Modal(document.getElementById('Alert'), {backdrop: true});
+
 // This url may need to change depending on what port your backend is running
 // on.
 const api = new API('http://localhost:5000');
@@ -9,3 +13,35 @@ const api = new API('http://localhost:5000');
 // Example usage of makeAPIRequest method.
 api.makeAPIRequest('dummy/user')
     .then(r => console.log(r));
+
+
+
+const showAlert = (alertTitle, alertText) => {
+    document.getElementById("alertTitle").innerText = alertTitle;
+    document.getElementById("alertText").innerText = alertText;
+
+    myModal.show();
+}
+
+const closeAlert = () => {
+    myModal.hide();
+}
+
+const login = (username, password) => {
+
+}
+
+login_form.addEventListener('submit', (event)=>{
+    
+    event.preventDefault();
+    
+    const username = login_form.elements.username.value;
+    const password = login_form.elements.password.value;
+    const con_password = login_form.elements.confirmPassword.value;
+    
+    if (password != con_password) {
+        showAlert("Error", "Confirm password incorrect!");
+    }
+
+    
+})
