200e9f8e4ece75c007c8d2ed9a14eb941c621af1
Henry Leung <henryliangzy@gmail.com>
2021-03-27 22:52:41 +1100

    finish 2.1.1

--- src/api.js
+++ src/api.js
@@ -3,10 +3,31 @@
  * @param {*} path The url to make the reques to.
  * @param {*} options Additiona options to pass to fetch.
  */
-const getJSON = (path, options) => 
-    fetch(path, options)
-        .then(res => res.json())
-        .catch(err => console.warn(`API_ERROR: ${err.message}`));
+ const getJSON = (path, options) => 
+ fetch(path, options)
+     .then(res => res.json())
+     .catch(err => console.warn(`API_ERROR: ${err.message}`));
+
+// const getJson = (path, options) => 
+// fetch(path, options)
+//     .then(res => res.json())
+//     .catch(err => err.message);
+
+// const get_json = (path, options) => {
+//     const result = fetch(path, options).then((data) => {
+//         console.log("data: ", data);
+//         if(data.status === 200){
+//             data.json().then((res) => {
+//                 return res;
+//             })
+//         }
+//         else{
+//             return data.status;
+//         }
+//     }).catch((err) => {
+//         return err;
+//     })
+// }
 
 /**
  * This is a sample class API which you may base your code on.
@@ -21,5 +42,21 @@ export default class API {
     /** @param {String} path */
     makeAPIRequest(path) {
         return getJSON(`${this.url}/${path}`);
+    }
+
+    /** 
+     * @param {String} path
+     * @param {json} data 
+     */
+    sendPostRequest(path, data) {
+        let option = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        return getJSON(`${this.url}/${path}`, option);
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,18 +2,47 @@ import API from './api.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
-
 let login_form = document.forms.login;
+let signup_form = document.forms.signup;
 let myModal = new bootstrap.Modal(document.getElementById('Alert'), {backdrop: true});
+const login_button = document.getElementById("login_button");
+const signup_button = document.getElementById("signup_button");
 
 // This url may need to change depending on what port your backend is running
 // on.
 const api = new API('http://localhost:5000');
 
 // Example usage of makeAPIRequest method.
-api.makeAPIRequest('dummy/user')
-    .then(r => console.log(r));
+// api.makeAPIRequest('dummy/user')
+//     .then(r => console.log(r));
 
+
+// const toLogin = () => {
+//     login_form.classList.remove("d-none");
+//     signup_form.classList.add("d-none");
+//     console.log("toLogin()");
+// }
+
+// const toSignUp = () => {
+//     signup_form.classList.remove("d-none");
+//     login_form.classList.add("d-none");
+//     console.log("toSignUp()");
+//}
+
+function toLogin () {
+    login_form.classList.remove("d-none");
+    signup_form.classList.add("d-none");
+    console.log("toLogin()");
+}
+
+function toSignUp () {
+    signup_form.classList.remove("d-none");
+    login_form.classList.add("d-none");
+    console.log("toSignUp()");
+}
+
+login_button.addEventListener("click", (event) => toLogin());
+signup_button.addEventListener("click", (event) => toSignUp());
 
 
 const showAlert = (alertTitle, alertText) => {
@@ -23,25 +52,36 @@ const showAlert = (alertTitle, alertText) => {
     myModal.show();
 }
 
-const closeAlert = () => {
-    myModal.hide();
-}
 
 const login = (username, password) => {
+    const login_info = {
+        "username": username,
+        "password": password
+    }
 
+    const result = api.sendPostRequest("auth/login", login_info).then((data) => {
+        if(data.token === undefined){
+            showAlert("Login Error", data.message);
+        }
+        else{
+            console.log(data.token);
+        }
+    })
 }
 
+
 login_form.addEventListener('submit', (event)=>{
-    
     event.preventDefault();
     
     const username = login_form.elements.username.value;
     const password = login_form.elements.password.value;
     const con_password = login_form.elements.confirmPassword.value;
     
-    if (password != con_password) {
+    if (password !== con_password) {
         showAlert("Error", "Confirm password incorrect!");
     }
-
-    
+    // if nothing wrong. login
+    else{
+        login(username, password);
+    }
 })
